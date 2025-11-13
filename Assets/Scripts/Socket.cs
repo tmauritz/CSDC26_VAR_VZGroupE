0135a66fcafbb7be7abc1a0f0c92d8312d776170
tmauritz <146174315+tmauritz@users.noreply.github.com>
2025-11-13 22:23:51 +0100

    Finally, some decent sockets.

--- Assets/Scripts/Socket.cs
+++ Assets/Scripts/Socket.cs
@@ -1,6 +1,7 @@
 using System;
 using Oculus.Interaction;
 using TMPro;
+using Unity.XR.CoreUtils;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
@@ -10,67 +11,67 @@ public class Socket : MonoBehaviour
     [SerializeField] public GameObject socketBase;
     [SerializeField] public Material baseMaterialPassive;
     [SerializeField] public Material baseMaterialActive;
-    [SerializeField] public LayerMask layer;
-    [SerializeField] public Rigidbody parentRigidbody;
-    
-    private bool objectInSocket = false;
-    private ConfigurableJoint joint = null;
-    
-    private GameObject targetGameObject;
-    
+    [SerializeField] public LayerMask attachableLayer;
+
+    private bool _objectInSocket = false;
+
+    private GameObject _targetGameObject;
+    private Transform _oldParent;
+    private Rigidbody _rigidbody;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(!objectInSocket && ((layer.value & (1 << other.gameObject.layer)) > 0))
+        if ((attachableLayer.value & (1 << other.gameObject.layer)) > 0)
         {
-            targetGameObject = other.gameObject;
-            Grabbable grabbable = targetGameObject.GetComponent<Grabbable>();
+            _targetGameObject = other.gameObject;
+            _oldParent = other.transform.parent;
+            Grabbable grabbable = _targetGameObject.GetComponent<Grabbable>();
 
             if (grabbable != null && (grabbable.SelectingPointsCount > 0))
             {
                 socketBase.GetComponent<MeshRenderer>().material = baseMaterialActive;
             }
         }
-
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if(!objectInSocket && ((layer.value & (1 << other.gameObject.layer)) > 0))
+        if ((attachableLayer.value & (1 << other.gameObject.layer)) > 0 && !_objectInSocket)
         {
-            Rigidbody targetRigidbody = targetGameObject.GetComponent<Rigidbody>();
             Grabbable grabbable = other.GetComponent<Grabbable>();
-            if (grabbable != null && (grabbable.SelectingPointsCount == 0) && !objectInSocket)
-            {   
-                joint = parentRigidbody.gameObject.AddComponent<ConfigurableJoint>();
-                joint.connectedBody = targetRigidbody;
-                joint.xMotion = ConfigurableJointMotion.Locked;
-                joint.yMotion = ConfigurableJointMotion.Locked;
-                joint.zMotion = ConfigurableJointMotion.Locked;
-                joint.angularXMotion = ConfigurableJointMotion.Locked;
-                joint.angularYMotion = ConfigurableJointMotion.Locked;
-                joint.angularZMotion = ConfigurableJointMotion.Locked;
-                joint.massScale = 1f;
-                joint.connectedMassScale = 1f;
-                joint.enableCollision = false;
-                objectInSocket = true;
+            if (grabbable != null && (grabbable.SelectingPointsCount == 0) && (_objectInSocket == false))
+            {
+                socket(_targetGameObject);
             }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        //only break the joint if the target object is grabbed
-        if (targetGameObject.GetComponent<Grabbable>().SelectingPointsCount > 0)
-        {
-            targetGameObject = null;
-            socketBase.GetComponent<MeshRenderer>().material = baseMaterialPassive;
-            clearJoint();
-        }
+        //if (other.gameObject != _targetGameObject) return;
+        unsocket(_targetGameObject);
+        _targetGameObject = null;
+        socketBase.GetComponent<MeshRenderer>().material = baseMaterialPassive;
     }
 
-    private void clearJoint()
+    private void socket(GameObject target)
     {
-        Destroy(joint);
-        objectInSocket = false;
+        if (target == null) return;
+        _targetGameObject = target;
+        Rigidbody targetRigidbody = target.GetComponent<Rigidbody>();
+        target.transform.parent = attachPoint.transform;
+        target.transform.position = attachPoint.transform.position;
+        target.transform.rotation = attachPoint.transform.rotation;
+        targetRigidbody.isKinematic = true;
+        _objectInSocket = true;
+    }
+
+    private void unsocket(GameObject target)
+    {
+        if (target == null) return;
+        Rigidbody targetRigidbody = target.GetComponent<Rigidbody>();
+        target.transform.parent = _oldParent;
+        targetRigidbody.isKinematic = false;
+        _objectInSocket = false;
     }
 }
